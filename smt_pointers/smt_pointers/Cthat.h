63182a0fc29372142d08c465cda316a955817ef2
stv <yarakaeff@gmail.com>
2014-10-06 15:41:17 +0400

    template +
    add template class

--- smt_pointers/smt_pointers/Cthat.h
+++ smt_pointers/smt_pointers/Cthat.h
@@ -12,9 +12,23 @@ class CPthat
 private:
 	Cthat* aThat;
 public:
-	CPthat(Cthat* _that=NULL):aThat(_that){};
+	CPthat(Cthat* _that= nullptr):aThat(_that){};
 	~CPthat(){ if (aThat) delete aThat;};
 	operator Cthat* (){return aThat;}; // Оператор преобразования типа
 	Cthat* operator->() {return aThat;}; // Оператор селектора ->
 	CPthat operator+(ptrdiff_t _offset) {return CPthat(aThat+_offset);};
+};
+
+template <class T>
+class sPointer
+{
+private:
+	T* tObj;
+public:
+	sPointer(T* _t= nullptr):tObj(_t){};
+	~sPointer(){if (tObj) delete tObj; };
+	operator T*() {return tObj;}
+	T* operator->(){return tObj;};
+
+
 };
